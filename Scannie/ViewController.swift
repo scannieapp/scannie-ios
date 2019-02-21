c2c46d3a39ed5cb293686a61817c2e912ff444f4
André Sousa <andre@iterar.co>
2019-02-21 20:10:42 +0000

    Initial Commit

---
+++ Scannie/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Scannie
+//
+//  Created by André Sousa on 21/02/2019.
+//  Copyright © 2019 Alves. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
